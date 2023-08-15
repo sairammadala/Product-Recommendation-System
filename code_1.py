f29fbd5747e9b8dfb511a38e507362e5836b0a19
sairammadala <sairammadala7101@gmail.com>
2023-08-15 23:01:47 +0530

    item-based model integrated

--- code_1.py
+++ code_1.py
@@ -7,84 +7,74 @@ import pandas as pd
 import scipy.stats
 import seaborn as sns
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
 
 
 # Rating dataset
 # Read And Handling missing values
-ratings = pd.read_csv("rating.csv")
+#ratings = pd.read_csv("rating.csv")
 
 #read product and merging dataset with rating
-product = pd.read_csv("product.csv")
-df = pd.merge(ratings,product, on= "ProductId" , how="inner")
+# product = pd.read_csv("product.csv")
+# df = pd.merge(ratings,product, on= "ProductId" , how="inner")
 
 
-#product with over 20 ratings
-agg_ratings= df.groupby("productName").agg(mean_ratings = ('rating' ,'mean'),
-                                        num_of_rating=('rating' , 'count')).reset_index()
+# #product with over 20 ratings
+# agg_ratings= df.groupby("productName").agg(mean_ratings = ('rating' ,'mean'),
+#                                         num_of_rating=('rating' , 'count')).reset_index()
 
-agg_ratings_GT20 = agg_ratings[agg_ratings['num_of_rating'] >1]
-agg_ratings_GT20.sort_values(by= 'num_of_rating' , ascending=False)
-df_GT20 = pd.merge(df,agg_ratings_GT20[['productName']] , on = 'productName' , how = 'inner')
+# agg_ratings_GT20 = agg_ratings[agg_ratings['num_of_rating'] >1]
+# agg_ratings_GT20.sort_values(by= 'num_of_rating' , ascending=False)
+# df_GT20 = pd.merge(df,agg_ratings_GT20[['productName']] , on = 'productName' , how = 'inner')
 
-#matrix
-matrix = df_GT20.pivot_table(index= 'productName' , columns='UserId' , values= 'rating' )
-print("/--------------------------------------------------------------------------------------------------")
-#data normalization
-matrix_norm = matrix.subtract(matrix.mean(axis= 1),axis = 'rows')
-#identify similar user
-item_similarity= matrix_norm.T.corr()
+# #matrix
+# matrix = df_GT20.pivot_table(index= 'productName' , columns='UserId' , values= 'rating' )
+# print("/--------------------------------------------------------------------------------------------------")
+# #data normalization
+# matrix_norm = matrix.subtract(matrix.mean(axis= 1),axis = 'rows')
+# #identify similar user
+# item_similarity= matrix_norm.T.corr()
 
-#print(matrix_norm.columns)
-picked_product= 'Apple'
-# Loop through not purchase product
-for picked_userid in matrix_norm.columns:
-        if pd.notnull(matrix_norm.loc[picked_product, picked_userid]):
-            # product that the target user has purchase
-            picked_userid_purchase = pd.DataFrame(matrix_norm[picked_userid].dropna(axis=0, how='all') \
-                                                 .sort_values(ascending=False)) \
-                                    .reset_index() \
-                                    .rename(columns={picked_userid: 'rating'})
-        
+# #print(matrix_norm.columns)
+# picked_product= 'Apple'
+# # Loop through not purchase product
+# for picked_userid in matrix_norm.columns:
+#         if pd.notnull(matrix_norm.loc[picked_product, picked_userid]):
+#             # product that the target user has purchase
+#             picked_userid_purchase = pd.DataFrame(matrix_norm[picked_userid].dropna(axis=0, how='all') \
+#                                                  .sort_values(ascending=False)) \
+#                                     .reset_index() \
+#                                     .rename(columns={picked_userid: 'rating'})
+df = pd.read_csv("./archive/dataset-itembased.csv")
+tfidf_vectorizer = TfidfVectorizer(stop_words='english')
+tfidf_matrix = tfidf_vectorizer.fit_transform(df['description'])
+cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)    
 
 import operator
 # Function to recommend product based on a picked product
-def recommend_product_based_on_product(picked_product, number_of_similar_items, number_of_recommendations):
-    # Similarity score of the picked product with all other product
-    picked_product_similarity_score = item_similarity[[picked_product]].reset_index().rename(columns={picked_product: 'similarity_score'})
-    #print(picked_product_similarity_score)
+def recommend_product_based_on_product(picked_product):
+    # Calculate TF-IDF for the entered keywords
+    tfidf_matrix_keywords = tfidf_vectorizer.transform([picked_product])
+    cosine_sim_keywords = linear_kernel(tfidf_matrix_keywords, tfidf_matrix).flatten()
     
-    rating_prediction = {}
+    
+    
+    # Get the indices of items that match the entered keywords
+    keyword_matching_indices = [[i,score] for i, score in enumerate(cosine_sim_keywords) if score > 0]
+    keyword_matching_indices = sorted(keyword_matching_indices, key=lambda x:x[1], reverse=True)
+    
+    # Create a DataFrame to store recommended items
+    recommended_items = pd.DataFrame(columns=df.columns)
+    
+    for item_idx in keyword_matching_indices:
+        recommended_items.loc[len(recommended_items)] = df.iloc[item_idx[0]]
 
-    # Loop through not purchase product
-    for picked_userid in matrix_norm.columns:
-        if pd.notnull(matrix_norm.loc[picked_product, picked_userid]):
-            # product that the target user has purchase
-            picked_userid_purchase = pd.DataFrame(matrix_norm[picked_userid].dropna(axis=0, how='all') \
-                                                 .sort_values(ascending=False)) \
-                                    .reset_index() \
-                                    .rename(columns={picked_userid: 'rating'})
+    #remaining code
             
-            # Rank the similarities between the picked user purchase product and the picked product.
-            picked_userid_purchase_similarity = pd.merge(left=picked_userid_purchase,
-                                                        right=picked_product_similarity_score,
-                                                        on='productName',
-                                                        how='inner') \
-                                                .sort_values('similarity_score', ascending=False)[:number_of_similar_items]
-            # Calculate the predicted rating using weighted average of similarity scores and the ratings from the picked user
-            predicted_rating = round(np.average(picked_userid_purchase_similarity['rating'],
-                                                weights=picked_userid_purchase_similarity['similarity_score']), 6)
-            # Save the predicted rating in the dictionary
-            rating_prediction[picked_userid] = predicted_rating
+    return recommended_items[:100]
 
-    # Sort the dictionary by predicted rating in descending order
-    sorted_predictions = sorted(rating_prediction.items(), key=operator.itemgetter(1), reverse=True)[:number_of_recommendations]
-
-    # Retrieve the product names for the recommended product
-    recommended_product = [(product.loc[product['ProductId'] == movie_id]['productName'].values[0], rating) for movie_id, rating in sorted_predictions]
-
-    #print(recommended_product)
-
-    return recommended_product
+    
 
 
 users = {
@@ -148,24 +138,7 @@ def search_recommand():
    if request.method == 'POST':
     search = request.form['search_text']
     picked_product= search
-    product_images = {
-      'Rice': url_for('static', filename='images/Rice.jpg'),
-      'Yogurt': url_for('static', filename='images/Yogurt.jpg'),
-      'Cucumber': url_for('static', filename='images/Cucumber.jpg'),
-      'Carrot': url_for('static', filename='images/Carrot.jpg'),
-      'Milk': url_for('static', filename='images/Milk.jpg'),
-      'Spinach': url_for('static', filename='images/Spinach.jpg'),
-      'Chicken': url_for('static', filename='images/Chicken.jpg'),
-      'Lettuce': url_for('static', filename='images/Lettuce.jpg'),
-       'Tomato': url_for('static', filename='images/Tomato.jpg'),
-       'Banana': url_for('static', filename='images/Banana.jpg'),
-        'Apple': url_for('static', filename='images/Apple.jpg'),
-        'Onion': url_for('static', filename='images/Onion.webp'),
-         'Bread': url_for('static', filename='images/Bread.webp') ,
-         'Orange': url_for('static', filename='images/Orange.webp') ,
-         'Potato': url_for('static', filename='images/Potato.jpg') 
-        }
-    
+
     #handling user ratings
     if request.form.get("product"):
         rated_product = request.form.get("product")
@@ -182,10 +155,23 @@ def search_recommand():
     else:
         print("oops")
 
-    recommended_product = recommend_product_based_on_product(picked_product=search, number_of_similar_items=2, number_of_recommendations=10)
-    recommended_product_names = [movie[0] for movie in recommended_product]
+    recommended_product = recommend_product_based_on_product(picked_product=search)
+
+    product_images = {}
+    recommended_product_names = {}
+    print(len(recommended_product))
+    items = []
+
+    for index, row in recommended_product.iterrows():
+        items.append(row['itemID'])
+        product_images[row['itemID']] = row['img_link']
+        recommended_product_names[row['itemID']] = row['product_name']
+    #print(recommended_product_names)
+    print(len(items))
+    print(len(set(items)))
+
     #print(recommended_product_names)  
-    return render_template('products.html',  picked_product=picked_product, recommended_product=recommended_product_names,product_images=product_images, product_ids_dict=product_ids_dict)
+    return render_template('products.html', recommended_product=recommended_product_names,product_images=product_images, product_ids_dict=product_ids_dict)
     
    return render_template('index.html')
     
